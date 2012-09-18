42238fe7cec91e277b42b8877fc4cb2f965acb67
kruser <ryan@kruseonline.net>
2012-09-18 10:18:37 -0500

    new project for etl from thetvdb

---
+++ etl-thetvdb/src/main/java/com/watchingstuff/etl/thetvdb/App.java
@@ -0,0 +1,13 @@
+package com.watchingstuff.etl.thetvdb;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
