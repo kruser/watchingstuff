0dc3d3872ccbaa3f6f7f61a7819f07a46f986436
kruser <ryan@kruseonline.net>
2012-09-18 23:03:17 -0500

    start the ETL app

---
+++ etl-thetvdb/src/main/java/com/watchingstuff/etl/thetvdb/UpdateTvDb.java
@@ -0,0 +1,12 @@
+package com.watchingstuff.etl.thetvdb;
+
+/**
+ * Synchronizes our database with the thetvdb.com based on the last synchronization
+ */
+public class UpdateTvDb 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Starting sync...." );
+    }
+}
